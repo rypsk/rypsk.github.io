1df662cc4e5a226bc8c1a7859b66e65c2345cb7b
Roberto Yébenes Paniagua <ryebenes.ext@eulen.com>
2023-07-07 15:03:33 +0200

    Se arregla REData para que muestre los precios por todas las horas del dia

--- src/app/modules/redata/services/redata.service.ts
+++ src/app/modules/redata/services/redata.service.ts
@@ -19,15 +19,19 @@ export class RedataService {
   }
 
   getData(requestDate: Date): Observable<any>{
-    let startDate = requestDate.toDateString() + 'T00:00:00';
-    let endDate = requestDate.toDateString() + 'T23:59:59';
+    let startDate = requestDate.toLocaleDateString() + 'T00:00:00';
+    // let endDate = requestDate.toDateString() + 'T23:59:59';
+    let end_date = new Date(requestDate.getTime() + (1000 * 60 * 60 * 24));
+    let endDate = end_date.toLocaleDateString() + 'T00:00:00';
     const url = 'https://api.esios.ree.es/indicators/1001';
+    // const url = 'https://apip.esios.ree.es/indicators/1001';
     const options = { 
       headers: new HttpHeaders().set('Accept', 'application/json; application/vnd.esios-api-v2+json')
       .set('Content-Type','application/json')
-      .set('Authorization','Token token=\"7084f590370f581565f184fec4930e6a4308d28d6688422a57af3e9f41084d78\"'),
+      // .set('Authorization','Token token=\"7084f590370f581565f184fec4930e6a4308d28d6688422a57af3e9f41084d78\"'),
+      .set('x-api-key','7084f590370f581565f184fec4930e6a4308d28d6688422a57af3e9f41084d78'),
       params: new HttpParams().set('geo_ids[]','8741').set('start_date',startDate).set('end_date',endDate)
-      // params: new HttpParams().set('geo_ids[]','8741').set('start_date','2021-07-12T00:00:00').set('end_date','2021-07-12T23:59:59')
+      // params: new HttpParams().set('geo_ids[]','8741').set('start_date','2023/07/07T00:00:00').set('end_date','2023/07/08T00:00:00')
     };
     console.log(url);
     return this.http.get(url, options)
